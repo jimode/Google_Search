f8fd35c1e2f4904da6a26c4221c63a615e893ae2
Jimi Odeyemi <jimode68@gmail.com>
2017-10-22 21:43:55 +0100

    A bit of refactoring

--- features/search_page.feature
+++ features/search_page.feature
@@ -5,7 +5,7 @@ I want to be able to carry out a search on google for an item
 
 Scenario: Search for an item
   Given I am on the google page
-  When I enter in a search word cucumber
+  When I enter in a search word "cucumber"
   Then I should see a search page results
-  And the search results titles shoud contain the word cucumber
-  And the search results description should contain the word cucumber 
+  And the search results titles shoud contain the word "cucumber"
+  And the search results description should contain the word "cucumber"
--- features/step_definitions/search_page.rb
+++ features/step_definitions/search_page.rb
@@ -3,13 +3,12 @@ Given("I am on the google page") do
     @search_page.load
     @results_page = Search.new
     @results_page.load
-    # expect(@search_page).to be_displayed
   end
   
-  When("I enter in a search word cucumber") do
-    @search_page.search_field.set 'cucumber'
+When("I enter in a search word {string}") do |search_word|
+    @search_page.search_field.set search_word
     @search_page.wait_until_search_drop_down_visible
-    index = @search_page.find_index_for('cucumber')
+    index = @search_page.find_index_for search_word
     @search_page.search_drop_down.options[index].click
 end
   
@@ -19,14 +18,14 @@ end
     expect(@results_page.results_container).to have_descriptions
   end
   
-  Then("the search results titles shoud contain the word cucumber") do
+  Then("the search results titles shoud contain the word {string}") do |search_word|
     @results_page.results_container.titles.each do |result_titles|
-        expect(result_titles.text.downcase).to include('cucumber')
+        expect(result_titles.text.downcase).to include(search_word)
     end
   end
   
-  Then("the search results description should contain the word cucumber") do
+  Then("the search results description should contain the word {string}") do |search_word|
     @results_page.results_container.titles.each do |result_descriptions|
-        expect(result_descriptions.text.downcase).to include('cucumber')
+        expect(result_descriptions.text.downcase).to include(search_word)
     end
   end
