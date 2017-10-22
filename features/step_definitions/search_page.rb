cb4da2d411b165a82713b5b2ff3c4802ce70529c
Jimi Odeyemi <jimode68@gmail.com>
2017-10-22 20:50:45 +0100

    Initial feature files and steps

---
+++ features/step_definitions/search_page.rb
@@ -0,0 +1,19 @@
+Given("I am on the google page") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When("I enter in a search word cucumber") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then("I should see a search page results") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then("the search results titles shoud contain the word cucumber") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then("the search results description should contain the word cucumber") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
